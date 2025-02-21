ed4fefc8b8d252d6ea4c911801a956a990561f62
KieranRodgerson <K.H.Rodgerson1@newcastle.ac.uk>
2025-02-21 15:40:58 +0000

    ImGui library files moved to a seperate project

--- CSC8508/UISystem.cpp
+++ CSC8508/UISystem.cpp
@@ -1,7 +1,7 @@
 #include "UISystem.h"
-#include "ImGui/imgui_impl_win32.h"
-#include "ImGui/imgui_impl_opengl3.h"
-#include "ImGui/imgui.h"
+#include "../ImGui/imgui_impl_win32.h"
+#include "../ImGui/imgui_impl_opengl3.h"
+#include "../ImGui/imgui.h"
 #include <filesystem>
 
 using namespace NCL;

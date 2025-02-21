f2c79a68d18e8c88dd4823b5e6eba15c3300c8b6
Alfie <alfiejay@mail.com>
2025-02-21 11:57:18 +0000

    Add EventType constraint

--- Event/Event.h
+++ Event/Event.h
@@ -4,10 +4,14 @@
 
 #ifndef EVENT_H
 #define EVENT_H
+#include <type_traits>
 
 /**
  * Base class for all events
  */
 class Event { };
+
+template <typename T>
+concept EventType = std::is_base_of_v<Event, T>;
 
 #endif //EVENT_H

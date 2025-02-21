c41cbf489677a06d098f8f6b38f9a2b8ee824101
Alasdair PB <39963762+Alasdair-PB@users.noreply.github.com>
2025-02-21 14:25:51 +0000

    DependentBindings sent as single packet

--- CSC8508CoreClasses/InputComponent.h
+++ CSC8508CoreClasses/InputComponent.h
@@ -14,18 +14,34 @@
 using std::vector;
 
 namespace NCL::CSC8508
-{
+{		
+	class InputButtonEvent : Event {
+		public:
+			InputButtonEvent(uint32_t eventData, GameObject& owner) : owner(owner) {
+				this->buttonId = buttonId;
+			}
+			uint32_t buttonId;
+			GameObject& owner;
+		protected:
+			uint32_t GetButtonId() const { return this->buttonId; }
+			GameObject& GetOwner() const { return this->owner; }
+	};
+
 	class InputComponent : public IComponent
 	{
 	public:
 		InputComponent(GameObject& gameObject, Controller* controller) : IComponent(gameObject), activeController(controller) {}
 		~InputComponent() = default;
 
+		virtual void OnAwake() override {
+			boundButtons = activeController->GetBoundButtons();
+		}
+
 		virtual void Update(float deltaTime) override
 		{
 			for (auto binding : boundButtons) {
-				if (activeController->GetButton(binding.first));
-					EventManager::Call(binding.second);
+				if (activeController->GetButton(binding));
+					CallInputEvent(binding);
 			}
 		}
 
@@ -34,7 +50,11 @@ namespace NCL::CSC8508
 		}
 
 	protected:
-		std::map<uint32_t, Event*> boundButtons;
+		void CallInputEvent(uint32_t button) {
+			InputButtonEvent inputEvent = InputButtonEvent(button, GetGameObject());
+			EventManager::Call<InputButtonEvent>(&inputEvent);
+		}
+		vector<uint32_t> boundButtons;
 		const Controller* activeController = nullptr;
 	};
 }

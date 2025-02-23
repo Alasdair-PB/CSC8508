251ea26cf76b25593ae5d832b286f4ad73c2a1be
TrulyHoax <92926117+ShimazakiRyou@users.noreply.github.com>
2025-02-23 22:31:49 +0000

    Update dungeon.cpp

--- CSC8508CoreClasses/dungeon.cpp
+++ CSC8508CoreClasses/dungeon.cpp
@@ -35,3 +35,15 @@ void Dungeon::generateRooms(int roomCount, int minSize, int maxSize) {
         }
     }
 }
+
+void Dungeon::connectRooms() {
+    // 可以按照房间中心点排序，然后依次连接
+    for (size_t i = 1; i < rooms.size(); ++i) {
+        Room* roomA = rooms[i - 1];
+        Room* roomB = rooms[i];
+        // 将 roomA 与 roomB 连接，比如将 roomB 加入 roomA 的邻接列表
+        roomA->neighbors.push_back(roomB);
+        roomB->neighbors.push_back(roomA);
+        // 此处可以添加走廊绘制的逻辑，例如记录走廊在网格中的占用单元
+    }
+}

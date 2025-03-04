6b696c94e550c846576a69b95aa80396693fb6b2
mlhumphriss <c1009080@newcastle.ac.uk>
2025-03-04 12:08:48 +0000

    Cleaned up comments in dungeon.h and room files

--- CSC8508/Room.cpp
+++ CSC8508/Room.cpp
@@ -4,37 +4,33 @@
 
 using namespace NCL::CSC8508::DungeonGen;
 
-// 主函数：运行地下城生成示例
-// ----------------------------
-int Room::maindun() {
-    // 设置地下城总体网格尺寸
+/// <summary>
+/// Creates a dungeon object using preset dungeonWidth and dungeonHeight. 
+/// This then generates rooms connected via corridors to create generated map.
+/// Currently spawns player in first room and has test movement
+/// </summary>
+/// <returns>Int 0</returns>
+int Room::MainDun() {
+
     const int dungeonWidth = 100;
     const int dungeonHeight = 100;
 
-    // 创建地下城对象
     Dungeon dungeon(dungeonWidth, dungeonHeight);
 
-    // 生成 10 个房间，房间尺寸在 5 到 15 之间
     dungeon.generateRooms(10, 5, 15);
-
-    // 连接房间（生成走廊）
     dungeon.connectRooms();
-
-    // 打印地下城信息
     dungeon.printDungeon();
 
-    // 创建玩家并放置在起始房间中心（假定第一个房间为起始房间）
+
     if (!dungeon.rooms.empty()) {
         auto [startX, startY] = dungeon.rooms.front()->center();
         Player player(startX, startY);
         std::cout << "Player starting at (" << startX << ", " << startY << ")" << std::endl;
 
-        // 示例：玩家移动
         player.move(1, 0);
         player.move(0, 1);
     }
 
-    // 运行完毕，等待用户输入后退出
     std::cout << "Press any key to exit..." << std::endl;
     std::cin.get();
     return 0;
--- CSC8508/Room.h
+++ CSC8508/Room.h
@@ -10,7 +10,7 @@ namespace NCL {
     namespace CSC8508 {
         namespace DungeonGen {
 
-            // 定义房间类型
+            
             enum class RoomType {
                 Start,
                 Monster,
@@ -20,31 +20,33 @@ namespace NCL {
             };
       
 
-            // 房间类定义
+            // Room type definition
+            
+            /// <summary>
+            /// Initiates Rooms and their type, as well as randomly generate their positions.
+            /// Can get room size, generates a room map
             class Room {
             public:
-                int x, y;           // 房间左上角坐标
-                int width, height;  // 房间宽高
-                RoomType type;      // 房间类型
-                std::vector<Room*> neighbors; // 邻接房间指针列表
+                int x, y;           
+                int width, height;  
+                RoomType type;      
+                std::vector<Room*> neighbors;
 
                 Room(int _x, int _y, int _w, int _h, RoomType _type = RoomType::Normal)
                     : x(_x), y(_y), width(_w), height(_h), type(_type) {
                 }
 
-                // 获取房间中心点
                 std::pair<int, int> center() const {
                     return { x + width / 2, y + height / 2 };
                 }
 
-                // 打印房间信息，方便调试
                 void print() const {
                     std::cout << "Room (" << x << ", " << y << ") "
                         << "Size: " << width << "x" << height
                         << " Type: " << static_cast<int>(type) << std::endl;
                 }
 
-				int maindun();
+				int MainDun();
             };
         }
     }

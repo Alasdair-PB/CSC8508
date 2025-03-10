16d4fa80a9758437f71936c84aed351d688f30ce
Alfie <alfiejay@mail.com>
2025-03-10 15:43:01 +0000

    Add basic room structure

---
+++ CSC8508/Map/RoomPrefab.h
@@ -0,0 +1,26 @@
+﻿//
+// Contributors: Alfie
+//
+
+#ifndef ROOMPREFAB_H
+#define ROOMPREFAB_H
+#include "GameObject.h"
+#include "NavigationMesh.h"
+
+using namespace NCL::CSC8508;
+
+/**
+ * Class that represents a room prefab
+ */
+class RoomPrefab {
+public:
+    RoomPrefab(GameObject* roomObject, NavigationMesh* navMesh) : roomObject(roomObject), navMesh(navMesh) { }
+
+protected:
+    GameObject* roomObject;
+    NavigationMesh* navMesh;
+    std::vector<Vector3> possibleItemSpawnLocations = std::vector<Vector3>(); // TODO: Replace with more spatially efficient structure
+    std::vector<Vector3> possibleDoorLocations = std::vector<Vector3>();
+};
+
+#endif //ROOMPREFAB_H

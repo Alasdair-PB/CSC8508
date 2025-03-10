72cd60f75a6bb8c1df2677a1f5c66bca0b954fa1
Alasdair PB <39963762+Alasdair-PB@users.noreply.github.com>
2025-03-10 15:23:30 +0000

    Changed cpp to h file and included file in core folder & cmake

---
+++ CSC8508/DamageableComponent.h
@@ -0,0 +1,73 @@
+#ifndef DAMAGEABLE_COMPONENT_H
+#define DAMAGEABLE_COMPONENT_H
+
+#include "IComponent.h"
+#include "Event.h"
+#include "EventManager.h"
+
+namespace NCL::CSC8508
+{
+    class DeathEvent : public Event {
+    private:
+        GameObject& gameObject;
+
+    public:
+        DeathEvent(GameObject& obj) : gameObject(obj) {}
+        GameObject& GetGameObject() { return gameObject; }
+    };
+
+    class DamageableComponent : public IComponent {
+    private:
+        int health;
+        int maxHealth;
+        GameObject& owner;
+
+    public:
+        DamageableComponent(GameObject& gameObject, int initialHealth, int initialMaxHealth)
+            : IComponent(gameObject), owner(gameObject),
+            health(std::max(0, initialHealth)),
+            maxHealth(std::max(1, initialMaxHealth)) {
+            health = std::min(health, maxHealth);
+        }
+
+        void DamageComponent(int damage) {
+            if (damage > 0) {
+                health = std::max(0, health - damage);
+                if (health <= 0)
+                    InvokeDeathEvent();
+            }
+        }
+
+        void HealComponent(int healthRegain) {
+            if (healthRegain > 0) {
+                health = std::min(maxHealth, health + healthRegain);
+            }
+        }
+
+        bool IsDestroyed() const {
+            return health == 0;
+        }
+
+        void SetMaxHealth(int newMaxHealth) {
+            if (newMaxHealth > 0) {
+                maxHealth = newMaxHealth;
+                health = std::min(health, maxHealth);
+            }
+        }
+
+        int GetHealth() const {
+            return health;
+        }
+
+        int GetMaxHealth() const {
+            return maxHealth;
+        }
+
+    private:
+        void InvokeDeathEvent() {
+            auto event = DeathEvent(owner);
+            EventManager::Call(&event);
+        }
+    };
+};
+#endif // DAMAGEABLE_COMPONENT_H

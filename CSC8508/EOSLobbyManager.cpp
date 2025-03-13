7792a6a1624d9f7175d6c0c42b25a4be965aa2dd
Alasdair PB <39963762+Alasdair-PB@users.noreply.github.com>
2025-03-13 16:00:40 +0000

    Removing shell access from CMake to build on ps5

--- CSC8508/EOSLobbyManager.cpp
+++ CSC8508/EOSLobbyManager.cpp
@@ -108,8 +108,8 @@ void EOSLobbyManager::OnLobbyCreated(const EOS_Lobby_CreateLobbyCallbackInfo* Da
         // Requests a modification handle to update lobby attributes
         EOS_EResult Result = EOS_Lobby_UpdateLobbyModification(eosLobbyManager.LobbyHandle, &ModOptions, &LobbyModificationHandle);
         if (Result == EOS_EResult::EOS_Success) {
+            const std::string PUBLIC_IP_ADDRESS = "128.366.543.43"; // fake ip
             std::cout << "[OnLobbyCreated] Lobby Modification Handle successfully created." << std::endl;
-
             std::cout << "Public IP Address: " << TOSTRING(PUBLIC_IP_ADDRESS) << std::endl;
 
             // Stores the lobby ID in the manager

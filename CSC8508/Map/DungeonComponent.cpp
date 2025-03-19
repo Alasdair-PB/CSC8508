ba8b3cbb0a7f6c4461895e7fe2dd2665268dd6a6
Alfie <alfiejay@mail.com>
2025-03-19 14:41:15 +0000

    Add placeholder functions

--- CSC8508/Map/DungeonComponent.cpp
+++ CSC8508/Map/DungeonComponent.cpp
@@ -3,3 +3,20 @@
 //
 
 #include "DungeonComponent.h"
+
+void DungeonComponent::Generate() {
+
+}
+
+
+RoomComponent* DungeonComponent::TryGenerateRoom(RoomPrefab const& prefab, RoomPrefab::DoorLocation const& doorLocation) {
+
+    // Make a new GameObject as a copy of the prefab's RoomObject
+    auto attemptedRoom = new GameObject(*prefab.GetRoomObject());
+
+    // Set the transform
+    //attemptedRoom->GetTransform().SetPosition().
+
+    return nullptr;
+}
+
--- CSC8508/Map/DungeonComponent.h
+++ CSC8508/Map/DungeonComponent.h
@@ -6,6 +6,7 @@
 #define DUNGEONCOMPONENT_H
 
 #include "IComponent.h"
+#include "RoomComponent.h"
 
 using namespace NCL::CSC8508;
 
@@ -13,8 +14,26 @@ class DungeonComponent final : public IComponent {
 public:
     explicit DungeonComponent(GameObject& gameObject) : IComponent(gameObject) { }
 
+    DungeonComponent(GameObject& gameObject, RoomPrefab::DoorLocation const& entrancePosition)
+        : IComponent(gameObject), entrancePosition(entrancePosition) { }
+
+    /**
+     * Procedurally generates rooms to fill the dungeon
+     */
+    void Generate();
+
 private:
     std::vector<GameObject*> rooms; // TODO: Maybe replace with RoomComponent? Whichever becomes more helpful
+
+    RoomPrefab::DoorLocation entrancePosition = RoomPrefab::DoorLocation(Vector3(0, 0, 0), Vector3(0, 0, -1));
+
+    /**
+     * Attempts to generate an attempted prefab room at a door location
+     * @param prefab Attempted RoomPrefab
+     * @param doorLocation Attempted world-space DoorLocation
+     * @return RoomComponent if successful, NULLPTR if not
+     */
+    RoomComponent* TryGenerateRoom(RoomPrefab const& prefab, RoomPrefab::DoorLocation const& doorLocation);
 };
 
 #endif //DUNGEONCOMPONENT_H

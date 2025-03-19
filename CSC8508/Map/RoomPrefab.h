8980dc662c9ba0284bfc3677170637695f14ba3f
Alfie <alfiejay@mail.com>
2025-03-19 14:41:59 +0000

    Add DoorLocation struct

--- CSC8508/Map/RoomPrefab.h
+++ CSC8508/Map/RoomPrefab.h
@@ -5,6 +5,7 @@
 #ifndef ROOMPREFAB_H
 #define ROOMPREFAB_H
 #include "AABBVolume.h"
+#include "Axis.h"
 #include "GameObject.h"
 #include "NavigationMesh.h"
 
@@ -12,15 +13,26 @@ using namespace NCL::CSC8508;
 
 class RoomPrefab {
 public:
+
+    struct DoorLocation {
+        Vector3 const pos;
+        Vector3 const dir;
+
+        DoorLocation(Vector3 const position, Vector3 const direction)
+            : pos(position), dir(direction) { }
+    };
+
+
     RoomPrefab(GameObject* roomObject, NavigationMesh* navMesh) : roomObject(roomObject), navMesh(navMesh) { }
 
+    [[nodiscard]] GameObject* GetRoomObject() const { return roomObject; }
     [[nodiscard]] NCL::AABBVolume GetEncasingVolume() const;
 
 protected:
     GameObject* roomObject;
     NavigationMesh* navMesh;
     std::vector<Vector3> possibleItemSpawnLocations = std::vector<Vector3>(); // TODO: Replace with more spatially efficient structure
-    std::vector<Vector3> possibleDoorLocations = std::vector<Vector3>();
+    std::vector<DoorLocation> possibleDoorLocations = std::vector<DoorLocation>();
 };
 
 #endif //ROOMPREFAB_H

8fbe1e6ae4217b1edfa34c74d4e51886e3d7e53b
Alfie <alfiejay@mail.com>
2025-03-24 17:18:27 +0000

    Add util for RandomiseVector method

---
+++ CSC8508/Util.cpp
@@ -0,0 +1,16 @@
+﻿//
+// Contributors: Alfie
+//
+
+#include <random>
+
+namespace Util {
+    template <typename T>
+    std::vector<T> RandomiseVector(std::vector<T> const& inVector) {
+        static std::random_device rd;
+        static std::default_random_engine rng(rd());
+        std::vector<T> out(inVector);
+        std::ranges::shuffle(out, rng);
+        return out;
+    }
+}

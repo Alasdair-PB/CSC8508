51d2adc1b54b9a1dc23983ea5006a90512760c2b
KieranRodgerson <K.H.Rodgerson1@newcastle.ac.uk>
2025-03-27 19:05:02 +0000

    Timer position moved

--- UISystem/TimerUI.h
+++ UISystem/TimerUI.h
@@ -11,7 +11,7 @@ namespace NCL {
 
 			void UpdateTimer(float t) { time = t; }
 
-			UIElementsGroup* timerUI = new UIElementsGroup(ImVec2(0.95f, 0.05f), ImVec2(0.08f, 0.08f), 1.0f, "Timer", 0.0f, ImGuiWindowFlags_NoResize);
+			UIElementsGroup* timerUI = new UIElementsGroup(ImVec2(0.9f, 0.05f), ImVec2(0.08f, 0.08f), 1.0f, "Timer", 0.0f, ImGuiWindowFlags_NoResize);
 
 		protected:
 			float time = 0;
